de9286f1a01fec6ba74859dccf88c325d0891cef
AkimJemi <rlawoals623@gmail.com>
2026-08-10 00:07:02 +0900

    20260810 test

--- tool/auto_create_file.sh
+++ tool/auto_create_file.sh
@@ -1,7 +1,9 @@
 TOOL_DIR=`pwd`
 WORK_DIR=`cd ..;echo $(pwd)`
-echo TOOL_DIR: $TOOL_DIR
-echo WORK_DIR: $WORK_DIR
+echo TOOL_DIR : $TOOL_DIR
+echo WORK_DIR : $WORK_DIR
 FILE_NAME=`find $WORK_DIR -name "$1*" -printf "%f\n"`
+echo FILE_NAME : $FILE_NAME
 "$SAKURA_EXE" "$FILE_NAME"
+
 
