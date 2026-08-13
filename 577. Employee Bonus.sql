3697d9b8ddb955f8495f5b16ca40f8035054edc3
AkimJemi <rlawoals623@gmail.com>
2026-08-13 21:52:53 +0900

    577. Employee Bonus

---
+++ 577. Employee Bonus.sql
@@ -0,0 +1,8 @@
+-- my oracle sql
+select ep.name, bonus from Employee ep left join bonus bn on ep.empId = bn.empId
+where bn.bonus < 1000 or bn.bonus is null;
+
+-- others sql
+select e.name , bonus from Employee E
+left outer join Bonus B on b.empid=E.empid
+where b.bonus<1000 Or b.bonus is null
